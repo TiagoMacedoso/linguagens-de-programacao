3c505fbb18aca569ec3f92aba95f8e4bdf7c9874
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2024-03-20 21:06:31 -0300

    Adição do meu código desenvolvido em minha quarta aula, trata-se da minha primeira interface criada em Java. É uma simples calculadora de IMC

---
+++ 0320_aula_04/interface_swing/src/main/java/br/facens/interface_swing/Interface_swing.java
@@ -0,0 +1,12 @@
+package br.facens.interface_swing;
+
+/**
+ *
+ * @author tiago
+ */
+public class Interface_swing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
